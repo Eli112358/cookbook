eab9ffe4bea9f4dc962cf24e26290b51ae74f44e
Eli112358 <Eli112358D@gmail.com>
2020-03-02 18:16:42 -0500

    Fix word wrap on mobile

--- root/js/tags.js
+++ root/js/tags.js
@@ -1,3 +1,9 @@
 function loadTags(recipe) {
-	return recipe.tags.map((tag) => createElement({name: "a", attributes: {href: `index.html?q=%23${tag}`}, content: [`#${tag}`]})); // %23 is #
+	let link = (tag) => createElement({
+		name: "a",
+		attributes: {href: `index.html?q=%23${tag}`}, // %23 is #
+		content: [`#${tag}`]
+	});
+	let span = () => createElement({name: "span", content: [" "]});
+	return recipe.tags.map(link).reduce((p, c) => p.concat(c, span()), [span()])
 }
